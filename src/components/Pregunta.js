9118f230d96a06cd965004eca18804b6e7778687
Camilo <camilo.gonzalez@bailacthor.com>
2021-04-25 15:53:53 -0400

    primer commit

---
+++ src/components/Pregunta.js
@@ -0,0 +1,30 @@
+import React, { Fragment, useState } from 'react';
+const Pregunta = () => {
+   //definir state
+    const [cantidad, guardarCantidad] = useState(0);
+
+    //Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        console.log(e.target.value);
+    }
+    return (
+        <Fragment>
+            <h2>Pon tu presupuesto</h2>
+            <form>
+                <input
+                 type="number"
+                 className="u-full-width"
+                 placeholder="Coloca tu presupuesto"
+                 onChange={definirPresupuesto} 
+                />
+                <input 
+                 type="submit"
+                 className="button-primary u-full-width"
+                 value="Definir Presupuesto"
+                />
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default Pregunta;
