e82acebaebbb3068f93ec3b6bf8459101d09aa48
Camilo <camilo.gonzalez@bailacthor.com>
2021-04-26 07:05:49 -0400

    componente Error creado

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,16 +1,34 @@
 import React, { Fragment, useState } from 'react';
+import Error from './Error';
 const Pregunta = () => {
    //definir state
     const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
 
     //Funcion que lee el presupuesto
     const definirPresupuesto = e => {
-        console.log(e.target.value);
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+    //Submit para definir presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+        if(cantidad <1 || NaN) {
+            guardarError(true);
+            return;
+        }
+        // si se pasa validación
+        guardarError(false);
     }
     return (
         <Fragment>
             <h2>Pon tu presupuesto</h2>
-            <form>
+            {error ? <Error mensaje="El presupuesto es incorrecto"/> : null }
+            <form
+             onSubmit={agregarPresupuesto}
+            >
                 <input
                  type="number"
                  className="u-full-width"
