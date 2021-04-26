8d2a7d9607bafaac59f9c927ed868101af3699d9
Camilo <camilo.gonzalez@bailacthor.com>
2021-04-26 08:02:03 -0400

    Flip change ui

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error';
-const Pregunta = ({guardarPresupuesto, guardarRestante}) => {
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
    //definir state
     const [cantidad, guardarCantidad] = useState(0);
     const [error, guardarError] = useState(false);
@@ -23,6 +23,7 @@ const Pregunta = ({guardarPresupuesto, guardarRestante}) => {
         guardarError(false);
         guardarPresupuesto(cantidad);
         guardarRestante(cantidad);
+        actualizarPregunta(false);
     }
     return (
         <Fragment>
