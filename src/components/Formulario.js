def649124bef711503fa8425e48da71ac18b6125
Camilo <camilo.gonzalez@bailacthor.com>
2021-04-26 08:41:41 -0400

    validaciones de formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState }from 'react';
+import Error from './Error';
 
 const Formulario = () => {
+    const [nombre, guardarNombre] = useState('');
+    const [cantidad, guardarCantidad] = useState(0);
+    const [ error, guardarError] = useState(false);
+
+    //cuando el usuario agrega un gasto
+    const agregarGasto = e => {
+        e.preventDefault();
+        // validar
+        if(cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
+            guardarError(true);
+            return;
+        }
+        //construir el gasto
+        //pasar al componente principal
+    }
+
     return ( 
-        <form>
+        <form 
+         onSubmit={agregarGasto}   
+        >
             <h2>Agrega tus gastos aquí</h2>
+            { error ? <Error mensaje="Ambos campos son obligatorios o presupuesto incorrecto"/> : null }
             <div>
                 <label>Nombre Gasto</label>
                 <input
-                    type="number"
+                    type="text"
                     className="u-full-width"
-                    placeholder="Ej. 300"
+                    placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
+                />
+            </div>
+            <div className="campo">
+                <label>Cantidad Gasto</label>
+                <input 
+                 type="number"
+                 className="u-full-width"
+                 placeholder="Ej. 300"
+                 value={cantidad}
+                 onChange={e => guardarCantidad( parseInt(e.target.value, 10) )}
                 />
             </div>
             <input 
